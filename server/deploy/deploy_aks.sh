58393c0c0e1db13263c49461381a241b1ad086f5
krisboorger <krisboorger@gmail.com>
2024-02-07 23:38:32 +0100

    fix(deploy): make tenant_id an envvar
    Make tenant_id an envvar, which will allow it to be later substituted
    when applying it with kubectl.

--- server/deploy/deploy_aks.sh
+++ server/deploy/deploy_aks.sh
@@ -18,7 +18,7 @@ helm repo add csi-secrets-store-provider-azure https://azure.github.io/secrets-s
 # save tf values for use with k8s
 cluster_name=$(terraform -chdir=terraform output -raw kubernetes_cluster_name)
 cluster_rg_name=$(terraform -chdir=terraform output -raw resource_group_name)
-tenant_id=$(terraform -chdir=terraform output -raw tenant_id)
+export tenant_id=$(terraform -chdir=terraform output -raw tenant_id)
 
 # switch to new k8s context
 az aks get-credentials --name $cluster_name --resource-group $cluster_rg_name
